60040603f22c78bf5e179a1ba0136bef02b4b38b
TakluBalm <jugalchap123@gmail.com>
2021-11-21 18:55:34 +0530

    Wrote and tested the PageRank algorithm

--- graph.c
+++ graph.c
@@ -5,6 +5,8 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#define error 0.005
+
 struct graph{
 	int vertices;
 	Sparse_matrix adj_matrix;
@@ -26,24 +28,45 @@ void DelGraph(graph g){
 
 //	Adds an edge from vertex u to vertex v
 void AddEdge(graph g, int u, int v){
-	AddValue(g->adj_matrix, 1, v, u);
+	AddValue(g->adj_matrix, 1, u, v);
 }
 
 //	Remove the edge from vertexx u to vertex v
 void RemoveEdge(graph g, int u, int v){
-	DelValue(g->adj_matrix, v, u);
+	DelValue(g->adj_matrix, u, v);
 }
 
-double* PageRank(graph g){
-	double* r = calloc(g->vertices, sizeof(double));
-	double* r_new = calloc(g->vertices, sizeof(double));
+float MOD(float a){
+	if(a < 0)	return (-1.0)*(a);
+	return a;
+}
+
+char isEqual(float* r1, float* r2, int size){
+	for(int i = 0; i < size; i++){
+		if(MOD(r1[i] - r2[i]) >= error)	return 0;
+	}
+	return 1;
+}
+
+float* PageRank(graph g){
+	float* r = calloc(g->vertices, sizeof(float));
+	float* r_new;
+	float* temp = calloc(g->vertices, sizeof(float));
 	Sparse_matrix M = DupMx(g->adj_matrix);
 	Normalize(M);
-	node cur = M->values;
+	node cur;
 
 	for(int i = 0; i < g->vertices; i++)	r[i] = 1.0/(g->vertices);
 
-	interation:
+	iteration:
+
+	if(isEqual(r, temp, g->vertices)){
+		free(temp);
+		return r;
+	}
+	free(temp);
+	cur = M->values;
+	r_new = calloc(g->vertices, sizeof(float));
 
 	for(int u = 0; u < M->rows; u++){
 		int num_el = M->row_index[u + 1] - M->row_index[u];
@@ -51,16 +74,13 @@ double* PageRank(graph g){
 		if(num_el == 0)	continue;
 
 		for(int i = 0; i < num_el; i++){
-			r_new[cur->col] += (cur->val)*r[cur->col];
+			r_new[cur->col] += (float)r[u]*(float)(cur->val);
 			cur = cur->next;
 		}
-
-		double* temp = r;
-		r = r_new;
-		r_new = temp;
-
-		goto interation;
 	}
+	temp = r;
+	r = r_new;
+	goto iteration;
 }
 
 #endif
